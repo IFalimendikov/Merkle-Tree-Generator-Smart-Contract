0f7dfc9f86a21682abc5474b177e23b31f17834d
Ivan Falimendikov <golovanjot2@gmail.com>
2023-02-06 21:09:55 +0100

    Add new functionality

--- merkle_tree.js
+++ merkle_tree.js
@@ -1,13 +1,10 @@
 const { MerkleTree, default: MerkleTree } = require('merkletreejs');
 const keccak256 = require("keccak256");
 
-let whitelistedAddresses = [
-  "",
-  ""
-]
+let whitelistedAddresses = require("./addresses.json");
 
 
 const leafNodes = whitelistedAddresses.map(addr => keccak256(addr));
-const MerkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
 
-const rootHash = MerkleTree.getRoot();
+const rootHash = merkleTree.getRoot();
