ce955f5e4e3f41fb990b7073c4d92ffd01e8a55c
Ivan Falimendikov <golovanjot2@gmail.com>
2023-02-25 23:50:42 +0100

    Cleaning code

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,11 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
-const { REACT_APP_GOERLI_RPC_URL, REACT_APP_MAINNET_RPC_URL, REACT_APP_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
 module.exports = {
    solidity: "0.8.4",
    networks: {
-      hardhat: {},
       goerli: {
          url: process.env.REACT_APP_GOERLI_RPC_URL,
          accounts: [process.env.REACT_APP_PRIVATE_KEY],
