a48ae911213d446921d84c4ae7e415a7134b6531
Ivan Falimendikov <golovanjot2@gmail.com>
2023-02-27 12:20:04 +0100

    Merkle tree gen init

---
+++ merkler_tree/merkle_tree.js
@@ -0,0 +1,12 @@
+const { MerkleTree} = require('merkletreejs');
+const keccak256 = require("keccak256");
+
+let whitelistedAddresses = require("./whitelist.json");
+
+
+const leafNodes = whitelistedAddresses.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+
+const rootHash = merkleTree.getRoot();
+
+console.log(`Merkle Tree\n`, merkleTree.toString());
